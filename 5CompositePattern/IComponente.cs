df22c9d605c187270cacf7137deaf1347147d0d8
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-20 06:57:27 -0500

    Composite Pattern

---
+++ 5CompositePattern/IComponente.cs
@@ -0,0 +1,15 @@
+﻿namespace _5CompositePattern
+{
+    public interface IComponente<T>
+    {
+        //Dato a guardar en los elementos
+        T Nombre { get; set; }
+
+        //  Comportamientos comunes
+
+        void Adicionar(IComponente<T> elemento);
+        IComponente<T> Borrar(T elemento);
+        IComponente<T> Buscar(T elemento);
+        string Mostrar(int profundidad);
+    }
+}
